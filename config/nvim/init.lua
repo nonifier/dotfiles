c252a19d0a16e6bbe5b7d07743a499e95f5f407c
Olivier Georget <ogeorget@prophesee.ai>
2024-09-05 10:19:18 +0200

    Config update for Lazyvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Configure `ruff-lsp`.
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
+-- For the default config, along with instructions on how to customize the settings
+require("lspconfig").ruff_lsp.setup({
+	init_options = {
+		settings = {
+			-- Any extra CLI arguments for `ruff` go here.
+			args = {},
+		},
+	},
+})
