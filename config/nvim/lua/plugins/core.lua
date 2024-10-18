324883b249d345dfbdef202d40ae1888951b6677
Olivier Georget <ogeorget@prophesee.ai>
2024-10-18 11:48:25 +0200

    Update Lazyvim config

---
+++ config/nvim/lua/plugins/core.lua
@@ -0,0 +1,18 @@
+return {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-macchiato",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				omnisharp = { enabled = false },
+			},
+		},
+	},
+	{ "Civitasv/cmake-tools.nvim", enable = false },
+}
